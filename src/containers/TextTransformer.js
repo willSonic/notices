1995f628dd4d3a6dccbbefdf1826a170c162dac4
Tyler Hughes <tyler@clearmetal.com>
2018-05-31 12:18:31 -0700

    Connect state to TextTransformer, display transformed text

--- src/containers/TextTransformer.js
+++ src/containers/TextTransformer.js
@@ -2,4 +2,9 @@ import { connect } from 'react-redux'
 import TextTransformer from '../components/TextTransformer'
 import { transformToLowerCase, transformToUpperCase } from '../store/textTransform'
 
-export default connect(null, { transformToLowerCase, transformToUpperCase })(TextTransformer)
+const mapStateToProps = state => ({
+    transformedValue: state.textTransform.transformedValue,
+    isLoading: state.textTransform.isLoading
+})
+
+export default connect(mapStateToProps, { transformToLowerCase, transformToUpperCase })(TextTransformer)
