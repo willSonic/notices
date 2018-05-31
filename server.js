b3a723280ddfca9c99ae177090de1ee146bd656b
Tyler Hughes <tyler@clearmetal.com>
2018-05-31 12:42:06 -0700

    Add random delay to API responses

--- server.js
+++ server.js
@@ -8,19 +8,29 @@ app.use(bodyparser.json())
 app.use(morgan('dev'))
 
 app.post('/api/lowercase', (req, res) => {
-    const isErrorResponse = Math.random() < 0.5
-    if (isErrorResponse) {
-        return res.status(500).json({ message: 'There was a very strange error.' })
-    }
-    return res.json({ output: req.body.input.toLowerCase() })
+    setTimeout(
+        () => {
+            const isErrorResponse = Math.random() < 0.5
+            if (isErrorResponse) {
+                return res.status(500).json({ message: 'There was a very strange error.' })
+            }
+            return res.json({ output: req.body.input.toLowerCase() })
+        },
+        (Math.ceil(Math.random())+1 * 1000)
+    )
 })
 
 app.post('/api/uppercase', (req, res) => {
-    const isErrorResponse = Math.random() < 0.5
-    if (isErrorResponse) {
-        return res.status(500).json({ message: 'There was a very strange error.' })
-    }
-    return res.json({ output: req.body.input.toUpperCase() })
+    setTimeout(
+        () => {
+            const isErrorResponse = Math.random() < 0.5
+            if (isErrorResponse) {
+                return res.status(500).json({ message: 'There was a very strange error.' })
+            }
+            return res.json({ output: req.body.input.toUpperCase() })
+        },
+        (Math.ceil(Math.random())+1 * 1000)
+    )
 })
 
 app.listen(3001, () => console.log('API is listening on port 3001.'))
